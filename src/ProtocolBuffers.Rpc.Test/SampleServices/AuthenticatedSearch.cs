8cf50fcce1c3a7cfbc343cf94b24ab2323453e6d
rogerk <rogerk@localhost>
2011-10-16 16:07:38 -0500

    Reformatted headers

--- src/ProtocolBuffers.Rpc.Test/SampleServices/AuthenticatedSearch.cs
+++ src/ProtocolBuffers.Rpc.Test/SampleServices/AuthenticatedSearch.cs
@@ -1,5 +1,4 @@
 ﻿#region Copyright 2010-2011 by Roger Knapp, Licensed under the Apache License, Version 2.0
-
 /* Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,9 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 #endregion
-
 using System;
 using Google.ProtocolBuffers.Rpc.Messages;
 using Google.ProtocolBuffers.TestProtos;
