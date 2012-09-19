be641c6f76204baa609a09179b1f28738c36b8fa
rogerk <rogerk@localhost>
2012-09-19 13:35:46 -0500

    fix sourced directory for 4.0 build

--- src/ProtocolBuffers.Rpc.Benchmarks/Properties/AssemblyInfo.cs
+++ src/ProtocolBuffers.Rpc.Benchmarks/Properties/AssemblyInfo.cs
@@ -22,7 +22,7 @@ using System.Reflection;
 [assembly: AssemblyCompany("Roger Knapp")]
 [assembly: AssemblyCopyright("Copyright 2011 by Roger Knapp, Licensed under the Apache License, Version 2.0")]
 
-[assembly: AssemblyVersion("1.12.919.10")]
-[assembly: AssemblyFileVersion("1.12.919.10")]
+[assembly: AssemblyVersion("1.12.919.11")]
+[assembly: AssemblyFileVersion("1.12.919.11")]
 
 [assembly: ObfuscateAssembly(false)]
