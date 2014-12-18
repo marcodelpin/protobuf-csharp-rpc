5e1f457edf91027e6b1bf5c87e3decd6137aa0b6
rogerk <rogerk@localhost>
2014-12-18 17:32:21 -0600

    Reorganizing for automation

--- build.bat
+++ build.bat
@@ -1,18 +1,1 @@
-@ECHO OFF
-
-REM ***** Update dependencies *****
-"%~dp0tools\NuGet.exe" install -x -OutputDirectory "%~dp0lib" NUnit -Version 2.5.10.11092
-"%~dp0tools\NuGet.exe" install -x -OutputDirectory "%~dp0lib" CSharpTest.Net.RpcLibrary -Version 14.327.1832.1051
-"%~dp0tools\NuGet.exe" install -x -OutputDirectory "%~dp0lib" Google.ProtocolBuffers -Version 2.4.1.521
-"%~dp0tools\NuGet.exe" install -x -OutputDirectory "%~dp0lib" Google.ProtocolBuffersLite -Version 2.4.1.521
-
-REM ***** Generate Source *****
-"%~dp0tools\CmdTool.exe" build src\*.csproj
-
-REM ***** Build All *****
-"%~dp0tools\CSBuild.exe" rebuild
-
-REM ***** Test *****
-"%~dp0lib\NUnit\tools\NUnit-Console.exe" /nologo /noshadow "%~dp0src\ProtocolBuffers.Rpc.Test\bin\Debug\Google.ProtocolBuffers.Rpc.Test.dll"
-
-ECHO.
+%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe /nologo ProtocolBuffers-RPC.sln /v:m /l:FileLogger,Microsoft.Build.Engine;logfile=MSBuild.log;verbosity=detailed /toolsversion:4.0 /t:Rebuild "/p:Configuration=Debug;Platform=Any CPU"
